299273319edc0aa4aa9cdd017683c6442712cc06
retsek9000 <30427@longroad.ac.uk>
2013-10-14 09:23:12 +0100

    First Iteration Commit
    infinite interger doubler added

---
+++ stuff.py
@@ -0,0 +1,10 @@
+def doubler():
+    
+    number = int(input("please enter a number:  "))
+    while number == number:
+        number = number + number
+        print (number)
+
+
+def matrix():
+#try to find rand number printer
